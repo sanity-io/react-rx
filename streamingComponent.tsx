61bf64770faecbdd0a5daa9ed1b40a10d64b8216
Bjørge Næss <bjoerge@gmail.com>
2018-10-21 22:22:14 +0200

    Flip which is built on which

--- streamingComponent.tsx
+++ streamingComponent.tsx
@@ -1,17 +1,58 @@
 import * as React from 'react'
-import {Observable} from 'rxjs'
-import {map} from 'rxjs/operators'
-import {withPropsStream} from './withPropsStream'
+import {Observable, Subject, Subscription} from 'rxjs'
 
-interface ElementProps {
-  element: React.ReactElement<any>
+type SetupFunction<SourceProps> =
+  | Observable<React.ReactNode>
+  | ((props$: Observable<SourceProps>) => Observable<React.ReactNode>)
+
+interface State {
+  node: React.ReactNode | null
 }
 
-export function streamingComponent<T>(
-  setup: (inputProps$: Observable<T>) => Observable<React.ReactElement<any>>
-) {
-  return withPropsStream<T, ElementProps>(
-    props$ => setup(props$).pipe(map(element => ({element}))),
-    props => props.element
-  )
+export function streamingComponent<SourceProps>(
+  setup: SetupFunction<SourceProps>
+): React.ComponentType<SourceProps> {
+  return class WithPropsStream extends React.Component<SourceProps, State> {
+    subscription: Subscription
+    props$: Subject<SourceProps> = new Subject()
+
+    constructor(props) {
+      super(props)
+
+      let isSync = true
+
+      const node$ = typeof setup === 'function' ? setup(this.props$.asObservable()) : setup
+
+      this.state = {node: null}
+
+      this.subscription = node$.subscribe(node => {
+        this.setStateMaybeSync({node}, isSync)
+      })
+
+      this.props$.next(this.props)
+
+      isSync = false
+    }
+
+    setStateMaybeSync = (nextState, isSync: boolean) => {
+      if (isSync) {
+        this.state = nextState
+      } else {
+        this.setState(nextState)
+      }
+    }
+
+    componentWillUnmount() {
+      this.subscription.unsubscribe()
+    }
+
+    // todo: figure out a future proof way of handling this
+    UNSAFE_componentWillReceiveProps(nextProps: SourceProps) {
+      this.props$.next(nextProps)
+    }
+
+    render() {
+      return this.state.node
+    }
+  }
 }
--- withPropsStream.tsx
+++ withPropsStream.tsx
@@ -1,62 +1,18 @@
 import * as React from 'react'
-import wrapDisplayName from 'recompose/wrapDisplayName'
-import {Observable, Subject, Subscription} from 'rxjs'
+import {Observable} from 'rxjs'
+import {map} from 'rxjs/operators'
+import {streamingComponent} from './streamingComponent'
 
-export type SetupFunction<SourceProps, TargetProps> =
+type SetupFunction<SourceProps, TargetProps> =
   | Observable<TargetProps>
   | ((props$: Observable<SourceProps>) => Observable<TargetProps>)
 
 export function withPropsStream<SourceProps, TargetProps>(
   setup: SetupFunction<SourceProps, TargetProps>,
   TargetComponent: React.ComponentType<TargetProps>
-): React.ComponentType<SourceProps> {
-  return class WithPropsStream extends React.Component<SourceProps, {targetProps: TargetProps}> {
-    static displayName = wrapDisplayName(TargetComponent, 'withPropsStream')
-
-    subscription: Subscription
-    props$: Subject<SourceProps> = new Subject()
-
-    constructor(props) {
-      super(props)
-
-      let isSync = true
-
-      const childProps$ = typeof setup === 'function' ? setup(this.props$.asObservable()) : setup
-
-      this.subscription = childProps$.subscribe(childProps => {
-        this.setChildProps(childProps, isSync)
-      })
-
-      this.props$.next(this.props)
-      isSync = false
-    }
-
-    setChildProps = (childProps: TargetProps, isSync: boolean) => {
-      if (typeof childProps !== 'object' || childProps === null) {
-        throw new Error(
-          'The observable emitted a non-object value. It should be an object that can be passed as react props.'
-        )
-      }
-
-      const state = {targetProps: childProps}
-      if (isSync) {
-        this.state = state
-      } else {
-        this.setState(state)
-      }
-    }
-
-    componentWillUnmount() {
-      this.subscription.unsubscribe()
-    }
-
-    // todo: figure out a future proof way of handling this
-    UNSAFE_componentWillReceiveProps(nextProps: SourceProps) {
-      this.props$.next(nextProps)
-    }
-
-    render() {
-      return this.state ? React.createElement(TargetComponent, this.state.targetProps) : null
-    }
-  }
+) {
+  return streamingComponent<SourceProps>(sourceProps$ => {
+    const targetProps$ = typeof setup === 'function' ? setup(sourceProps$) : setup
+    return targetProps$.pipe(map(props => <TargetComponent {...props} />))
+  })
 }
