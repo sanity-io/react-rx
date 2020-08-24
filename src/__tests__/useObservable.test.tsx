6e5cb671417ce8d8a9d32d76bf7b29cbb07deb61
Bjørge Næss <bjoerge@gmail.com>
2020-08-24 14:33:40 +0200

    Make useObservable resubscribe when recieving new observable

--- src/__tests__/useObservable.test.tsx
+++ src/__tests__/useObservable.test.tsx
@@ -20,6 +20,21 @@ test('should subscribe immediately and unsubscribe on component unmount', () => 
   expect(subscribed).toBe(false)
 })
 
+test('should only subscribe once when given same observable on rerenders', () => {
+  let subscriptionCount = 0
+  const observable = new Observable(() => {
+    subscriptionCount++
+  })
+
+  expect(subscriptionCount).toBe(0)
+
+  const {unmount, rerender} = renderHook(() => useObservable(observable))
+  expect(subscriptionCount).toBe(1)
+  rerender()
+  expect(subscriptionCount).toBe(1)
+  unmount()
+})
+
 test('should have sync values from an observable as initial value', () => {
   const {result} = renderHook(() => useObservable(of('something sync')))
   expect(result.current).toBe('something sync')
@@ -52,5 +67,34 @@ test('should update with values from observables', () => {
 
   act(() => values$.next('otherthing'))
   expect(result.current).toBe('otherthing')
+  unmount()
+})
+
+test('should re-subscribe when receiving a new observable', () => {
+  const first$ = new Subject<string>()
+  const second$ = new Subject<string>()
+
+  let current$ = first$
+
+  const {result, rerender, unmount} = renderHook(() => useObservable(current$))
+
+  act(() => first$.next('first 1'))
+  expect(result.current).toBe('first 1')
+
+  current$ = second$
+
+  rerender()
+
+  // since observable #2 hasn't emitted a value yet, we should still have the previous
+  expect(result.current).toBe('first 1')
+
+  // Now we should be subscribed to second$ and it's emission should be returned
+  act(() => second$.next('second 1'))
+  expect(result.current).toBe('second 1')
+
+  // we should no longer be subscribed to the first and ignore any emissions
+  act(() => first$.next('first 2'))
+  expect(result.current).toBe('second 1')
+
   unmount()
 })
--- src/useObservable.ts
+++ src/useObservable.ts
@@ -32,11 +32,11 @@ export function toObservable<T, K>(
   return setup ? setup(observable) : observable
 }
 
-const isFunction = (val: any): val is Function => typeof val === 'function'
+const isCallable = (val: any): val is (...args: unknown[]) => unknown => typeof val === 'function'
 
 function getValue<T>(value: T): T
 function getValue<T>(value: T | (() => T)): T {
-  return isFunction(value) ? value() : value
+  return isCallable(value) ? value() : value
 }
 
 export function useObservable<T>(observable$: Observable<T>): T | undefined
@@ -44,6 +44,7 @@ export function useObservable<T>(observable$: Observable<T>, initialValue: T): T
 export function useObservable<T>(observable$: Observable<T>, initialValue: () => T): T
 export function useObservable<T>(observable$: Observable<T>, initialValue?: T): T | undefined {
   const subscription = React.useRef<Subscription>()
+  const isInitial = React.useRef(true)
   const [value, setState] = React.useState<T | undefined>(() => {
     let isSync = true
     let syncVal = getValue(initialValue)
@@ -58,14 +59,18 @@ export function useObservable<T>(observable$: Observable<T>, initialValue?: T): 
     return syncVal
   })
 
-  React.useEffect(
-    () => () => {
+  React.useEffect(() => {
+    // when the observable$ changes after initial (possibly sync render)
+    if (!isInitial.current) {
+      subscription.current = observable$.subscribe(nextVal => setState(nextVal))
+    }
+    isInitial.current = false
+    return () => {
       if (subscription.current) {
         subscription.current.unsubscribe()
       }
-    },
-    [],
-  )
+    }
+  }, [observable$])
 
   return value
 }
