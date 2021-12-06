b4dd87a75dbb84b711bc527dd97cd54165a89342
Bjørge Næss <bjoerge@gmail.com>
2021-12-06 23:52:32 +0100

    chore(website): render root in React strict mode

--- website/src/entries/main.tsx
+++ website/src/entries/main.tsx
@@ -3,4 +3,9 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import {App} from '../App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('app')
+)
