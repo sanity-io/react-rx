0f8b869216ee5a7dd69c1c6fcbf2bf34835e9c80
Bjørge Næss <bjoerge@gmail.com>
2022-06-16 18:08:19 +0200

    refactor: simplify useObservable and fix issue with not returning initial value

--- src/useObservable.ts
+++ src/useObservable.ts
@@ -1,51 +1,29 @@
-import {defer, merge, Observable, of, pipe} from 'rxjs'
+import {Observable} from 'rxjs'
 import {DependencyList, useMemo} from 'react'
-import {useAsObservable} from './useAsObservable'
-import {distinctUntilChanged, switchMap} from 'rxjs/operators'
 import {useSyncExternalStore} from 'use-sync-external-store/shim'
 
 function getValue<T>(value: T): T extends () => infer U ? U : T {
   return typeof value === 'function' ? value() : value
 }
 
-function useObservableSubscription<T>(observable: Observable<T>): T {
-  const store = useMemo(() => {
-    let currentValue: T
-    return {
-      // Note: this works because the given observable always emits a value synchronously by concat-ing the given initialValue
-      getCurrentValue: () => currentValue,
-      subscribe: (callback: (value: T) => void) => {
-        const subscription = observable.subscribe(value => {
-          currentValue = value
-          callback(value)
-        })
-        return () => {
-          subscription.unsubscribe()
-        }
-      },
-    }
-  }, [observable])
-  return useSyncExternalStore(store.subscribe, store.getCurrentValue)
-}
-
 export function useObservable<T>(observable: Observable<T>): T | undefined
 export function useObservable<T>(observable: Observable<T>, initialValue: T): T
 export function useObservable<T>(observable: Observable<T>, initialValue: () => T): T
 export function useObservable<T>(observable: Observable<T>, initialValue?: T | (() => T)) {
-  return useObservableSubscription(
-    useAsObservable(
-      observable,
-      pipe(
-        distinctUntilChanged(),
-        switchMap(observable =>
-          merge(
-            defer(() => of(getValue(initialValue))),
-            observable,
-          ),
-        ),
-      ),
-    ),
-  )
+  const [getSnapshot, subscribe] = useMemo(() => {
+    let currentValue = getValue(initialValue)
+    return [
+      () => currentValue,
+      (callback: (value: T) => void) => {
+        const subscription = observable.subscribe(value => {
+          currentValue = value
+          callback(value)
+        })
+        return () => subscription.unsubscribe()
+      },
+    ]
+  }, [observable])
+  return useSyncExternalStore(subscribe, getSnapshot)
 }
 
 export function useMemoObservable<T>(
