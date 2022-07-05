e5aae9bbc3aaf2073371f58fefeb72cb406301e2
Bjørge Næss <bjoerge@gmail.com>
2022-07-05 15:25:36 +0200

    website: add debug entry file

---
+++ website/src/entries/debug.tsx
@@ -0,0 +1,29 @@
+import './main.css'
+import * as React from 'react'
+import {useObservable, useAsObservable} from 'react-rx'
+import {Fragment, useState} from 'react'
+import {createRoot} from 'react-dom/client'
+
+function Test(props: {count: number}) {
+  const value = useObservable(useAsObservable(props.count))
+  console.log(value)
+  return <div>OK {value}</div>
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Fragment>
+      <button onClick={() => setCount((c) => c + 1)}>Click</button>
+      {count % 2 === 0 && <Test count={count} />}
+      <div>Count: {count}</div>
+    </Fragment>
+  )
+}
+const root = createRoot(document.getElementById('app')!)
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
