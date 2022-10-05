14969c238b25a974b57ed7f5137fbf891917e442
Cody Olsen <stipsan@gmail.com>
2022-10-05 13:03:58 +0200

    fix: react strict mode compatibility (#7)
    Co-authored-by: Cody Olsen <stipsan@gmail.com>
    Co-authored-by: Bjørge Næss <bjoerge@gmail.com>

--- src/useObservable.ts
+++ src/useObservable.ts
@@ -1,8 +1,7 @@
 import {Observable, Subscription} from 'rxjs'
-import {DependencyList, useMemo} from 'react'
+import {DependencyList, useEffect, useMemo, useRef} from 'react'
 import {useSyncExternalStore} from 'use-sync-external-store/shim'
 import {shareReplay, tap} from 'rxjs/operators'
-import {useIsomorphicEffect} from './useIsomorphicEffect'
 
 function getValue<T>(value: T): T extends () => infer U ? U : T {
   return typeof value === 'function' ? value() : value
@@ -22,25 +21,31 @@ function getOrCreateStore<T>(inputObservable: Observable<T>, initialValue: T) {
       shareReplay({refCount: true, bufferSize: 1}),
       tap(value => (entry.currentValue = value)),
     )
+
+    // Eagerly subscribe to sync set `entry.currentValue` to what the observable returns
     entry.subscription = entry.observable.subscribe()
 
     cache.set(inputObservable, entry as CacheRecord<T>)
   }
-  return cache.get(inputObservable)
+  return cache.get(inputObservable)!
 }
 
 export function useObservable<T>(observable: Observable<T>): T | undefined
 export function useObservable<T>(observable: Observable<T>, initialValue: T): T
 export function useObservable<T>(observable: Observable<T>, initialValue: () => T): T
 export function useObservable<T>(observable: Observable<T>, initialValue?: T | (() => T)) {
-  const [getSnapshot, subscribe] = useMemo(() => {
-    const record = getOrCreateStore(observable, getValue(initialValue))!
+  const [getSnapshot, subscribe] = useMemo<
+    [() => T, Parameters<typeof useSyncExternalStore>[0]]
+  >(() => {
+    const store = getOrCreateStore(observable, getValue(initialValue))
     return [
       function getSnapshot() {
-        return record.currentValue
+        // @TODO: perf opt opportunity: we could do `store.subscription.unsubscribe()` here to clear up some memory, as this subscription is only needed to provide a sync initialValue.
+        return store.currentValue
       },
-      function subscribe(callback: (value: T) => void) {
-        const sub = record.observable.subscribe(next => callback(next))
+      function subscribe(callback: () => void) {
+        // @TODO: perf opt opportunity: we could do `store.subscription.unsubscribe()` here as we only need 1 subscription active to keep the observer alive
+        const sub = store.observable.subscribe(callback)
         return () => {
           sub.unsubscribe()
         }
@@ -48,9 +53,21 @@ export function useObservable<T>(observable: Observable<T>, initialValue?: T | (
     ]
   }, [observable])
 
-  useIsomorphicEffect(() => {
+  const shouldRestoreSubscriptionRef = useRef(false)
+  useEffect(() => {
+    const store = getOrCreateStore(observable, getValue(initialValue))
+    if (shouldRestoreSubscriptionRef.current) {
+      if (store.subscription.closed) {
+        store.subscription = store.observable.subscribe()
+      }
+      shouldRestoreSubscriptionRef.current = false
+    }
+
     return () => {
-      getOrCreateStore(observable, getValue(initialValue))!.subscription.unsubscribe()
+      // React StrictMode will call effects as `setup + teardown + setup` thus we can't trust this callback as "react is about to unmount"
+      // Tracking this ref lets us set the subscription back up on the next `setup` call if needed, and if it really did unmounted then all is well
+      shouldRestoreSubscriptionRef.current = !store.subscription.closed
+      store.subscription.unsubscribe()
     }
   }, [observable])
 
