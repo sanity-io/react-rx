a4e4d3e904d8e153f01171aba7d65aabd7160330
Bjørge Næss <bjoerge@gmail.com>
2022-10-06 11:14:37 +0200

    chore: remove unused file

--- src/useIsomorphicEffect.ts
+++
@@ -1,6 +0,0 @@
-import {useEffect, useLayoutEffect} from 'react'
-
-/**
- * @deprecated it's better to `useEffect`
- */
-export const useIsomorphicEffect = typeof window !== 'undefined' ? useLayoutEffect : useEffect
