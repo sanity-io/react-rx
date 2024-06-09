f5a96e602ca44080af318f45a52cfce203e4daa6
Cody Olsen <stipsan@gmail.com>
2024-06-09 21:15:43 +0200

    refactor: remove `useObservableCallback`
    Use `useObservableEvent` instead

--- src/useObservableEvent.ts
+++ src/useObservableEvent.ts
@@ -1,32 +1,6 @@
 import {observableCallback} from 'observable-callback'
-import {DependencyList, useCallback, useEffect, useInsertionEffect, useRef, useState} from 'react'
+import {useCallback, useEffect, useInsertionEffect, useRef, useState} from 'react'
 import {Observable} from 'rxjs'
-
-const EMPTY_DEPS: DependencyList = []
-
-export function useObservableCallback<T, U>(
-  fn: (arg: Observable<T>) => Observable<U>,
-  dependencies: DependencyList = EMPTY_DEPS,
-): (arg: T) => void {
-  const callbackRef = useRef<[Observable<T>, (val: T) => void]>()
-
-  if (!callbackRef.current) {
-    callbackRef.current = observableCallback<T>()
-  }
-
-  const [calls$, call] = callbackRef.current
-
-  const callback = useCallback(fn, dependencies)
-
-  useEffect(() => {
-    const subscription = calls$.pipe(callback).subscribe()
-    return () => {
-      subscription.unsubscribe()
-    }
-  }, [calls$, call, callback])
-
-  return call
-}
 
 export function useObservableEvent<T, U>(
   fn: (arg: Observable<T>) => Observable<U>,
