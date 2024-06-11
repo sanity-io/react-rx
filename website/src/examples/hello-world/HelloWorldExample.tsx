b86943c254f981aa99ea039fa802c610b68d4db7
Cody Olsen <stipsan@gmail.com>
2024-06-11 23:58:56 +0200

    docs: merging and simplifying some examples

--- website/src/examples/hello-world/HelloWorldExample.tsx
+++ website/src/examples/hello-world/HelloWorldExample.tsx
@@ -1,12 +1,9 @@
-import {useMemo} from 'react'
 import {useObservable} from 'react-rx'
 import {of} from 'rxjs'
 
+const observable = of('World')
+
 export default function App() {
-  const observable = useMemo(
-    () => of('World'),
-    [],
-  )
   const data = useObservable(observable)
 
   return <h1>Hello, {data}!</h1>
