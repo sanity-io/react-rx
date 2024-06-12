2bfab288abe371781ac7e728d42bf4202c8b4ba4
Cody Olsen <stipsan@gmail.com>
2024-06-12 02:38:06 +0200

    docs: cleanup

--- website/src/examples/form-data/FormDataExample.tsx
+++ website/src/examples/form-data/FormDataExample.tsx
@@ -1,41 +1,16 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import * as RxJS from 'rxjs'
-import * as operators from 'rxjs/operators'
-
-const {of, from, concat, merge} = RxJS
-const {
-  timer,
-  interval,
-  throwError,
-  combineLatest,
-  Observable,
-} = RxJS
-
-const {map, filter, reduce, scan, tap} = operators
-const {concatMap, mergeMap, switchMap, mapTo} =
-  operators
-const {startWith, catchError, take} = operators
-//@endimport
-
-import {observableCallback} from 'observable-callback'
+import {handler, rxComponent} from 'react-rx-old'
+import {concat, merge, timer} from 'rxjs'
 import {
-  context,
-  elementRef,
-  forwardRef,
-  handler,
-  rxComponent,
-  state,
-  useAsObservable,
-  useMemoObservable,
-  useObservable,
-} from 'react-rx-old'
-import styled from 'styled-components'
+  concatMap,
+  map,
+  scan,
+  startWith,
+  tap,
+  withLatestFrom,
+} from 'rxjs/operators'
+import {styled} from 'styled-components'
 
 import storage from './storage'
-//@endimport
-
-const {withLatestFrom} = operators
 
 const STORAGE_KEY = '__form-submit-example__'
 
@@ -172,6 +147,7 @@ const Form = styled.form`
   }
   input,
   textarea {
+    box-sizing: border-box;
     width: 100%;
     padding: 5px;
   }
