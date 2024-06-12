8e42c1d33178d47f1b4ac57efb7c164f1e0f5b7e
Cody Olsen <stipsan@gmail.com>
2024-06-12 02:14:46 +0200

    docs: fix FizzBuzz

--- website/src/examples/fizz-buzz/FizzBuzzExample.tsx
+++ website/src/examples/fizz-buzz/FizzBuzzExample.tsx
@@ -1,59 +1,29 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import * as RxJS from 'rxjs'
-import * as operators from 'rxjs/operators'
+import {useObservable} from 'react-rx'
+import {timer} from 'rxjs'
+import {map, take} from 'rxjs/operators'
 
-const {of, from, concat, merge} = RxJS
-const {
-  timer,
-  interval,
-  throwError,
-  combineLatest,
-  Observable,
-} = RxJS
-
-const {map, filter, reduce, scan, tap} = operators
-const {concatMap, mergeMap, switchMap, mapTo} =
-  operators
-const {startWith, catchError, take} = operators
-//@endimport
-
-import {observableCallback} from 'observable-callback'
-import {
-  context,
-  elementRef,
-  forwardRef,
-  handler,
-  rxComponent,
-  state,
-  useAsObservable,
-  useMemoObservable,
-  useObservable,
-} from 'react-rx-old'
-
-const FizzBuzzExample = rxComponent(
-  timer(0, 500).pipe(
-    map((n) => n + 1),
-    map((n) => {
-      const divBy3 = n % 3 === 0
-      const divBy5 = n % 5 === 0
-      const divBy3And5 = divBy3 && divBy5
-      return divBy3And5
-        ? 'Fizz Buzz'
-        : divBy3
-          ? 'Fizz'
-          : divBy5
-            ? 'Buzz'
-            : String(n)
-    }),
-    // map((seq: string[], curr) => seq.concat(curr), []),
-    map((n, i) => (
-      <div key={n + i}>
-        {i + 1}: {n}
-      </div>
-    )),
-    take(100),
-  ),
+const observable = timer(0, 500).pipe(
+  map((n) => n + 1),
+  map((n) => {
+    const divBy3 = n % 3 === 0
+    const divBy5 = n % 5 === 0
+    const divBy3And5 = divBy3 && divBy5
+    return divBy3And5
+      ? 'Fizz Buzz'
+      : divBy3
+        ? 'Fizz'
+        : divBy5
+          ? 'Buzz'
+          : String(n)
+  }),
+  map((n, i) => (
+    <h1>
+      {i + 1}: {n}
+    </h1>
+  )),
+  take(100),
 )
 
-export default FizzBuzzExample
+export default function App() {
+  return useObservable(observable)
+}
