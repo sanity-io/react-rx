ec8df1838f5dba8a6f9df98ad5a080b269c64593
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-07-10 13:14:42 +0200

    fix: use `use-effect-event` (#106)

--- src/useObservableEvent.ts
+++ src/useObservableEvent.ts
@@ -1,6 +1,7 @@
 import {observableCallback} from 'observable-callback'
-import {useCallback, useEffect, useInsertionEffect, useRef, useState} from 'react'
+import {useEffect, useState} from 'react'
 import {type Observable} from 'rxjs'
+import {useEffectEvent} from 'use-effect-event'
 
 /** @public */
 export function useObservableEvent<T, U>(
@@ -16,26 +17,4 @@ export function useObservableEvent<T, U>(
   }, [callback, calls$])
 
   return call
-}
-
-/**
- * This is a ponyfill of the upcoming `useEffectEvent` hook that'll arrive in React 19.
- * https://19.react.dev/learn/separating-events-from-effects#declaring-an-effect-event
- * To learn more about the ponyfill itself, see: https://blog.bitsrc.io/a-look-inside-the-useevent-polyfill-from-the-new-react-docs-d1c4739e8072
- */
-function useEffectEvent<
-  const T extends (
-    ...args: // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    any[]
-  ) => void,
->(fn: T): T {
-  const ref = useRef<T | null>(null)
-  useInsertionEffect(() => {
-    ref.current = fn
-  }, [fn])
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return useCallback((...args: any[]) => {
-    const f = ref.current!
-    return f(...args)
-  }, []) as T
 }
