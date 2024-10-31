adc60eab739e450ab46e22872e5c60748a0e0d26
Cody Olsen <81981+stipsan@users.noreply.github.com>
2024-10-31 11:44:24 +0100

    fix: refactor initial value to not need a useRef + useEffect loop (#143)

--- src/useObservable.ts
+++ src/useObservable.ts
@@ -1,4 +1,4 @@
-import {useEffect, useMemo, useRef, useSyncExternalStore} from 'react'
+import {useMemo, useSyncExternalStore} from 'react'
 import {
   asapScheduler,
   catchError,
@@ -42,48 +42,35 @@ export function useObservable<ObservableType extends Observable<any>, InitialVal
   observable: ObservableType,
   initialValue?: InitialValue | (() => InitialValue),
 ): InitialValue | ObservedValueOf<ObservableType> {
-  /**
-   * Store the initialValue in a ref, as we don't want a changed `initialValue` to trigger a re-subscription.
-   * But we also don't want the initialValue to be stale if the observable changes.
-   */
-  const initialValueRef = useRef(getValue(initialValue) as ObservedValueOf<ObservableType>)
+  if (!cache.has(observable)) {
+    const entry: Partial<CacheRecord<ObservedValueOf<ObservableType>>> = {
+      snapshot: getValue(initialValue) as ObservedValueOf<ObservableType>,
+    }
+    entry.observable = observable.pipe(
+      map((value) => ({snapshot: value, error: undefined})),
+      catchError((error) => of({snapshot: undefined, error})),
+      tap(({snapshot, error}) => {
+        entry.snapshot = snapshot
+        entry.error = error
+      }),
+      // Note: any value or error emitted by the provided observable will be mapped to the cache entry's mutable state
+      // and the observable is thereafter only used as a notifier to call `onStoreChange`, hence the `void` return type.
+      map((value) => void value),
+      // Ensure that the cache entry is deleted when the observable completes or errors.
+      finalize(() => cache.delete(observable)),
+      share({resetOnRefCountZero: () => timer(0, asapScheduler)}),
+    )
 
-  /**
-   * Ensures that the initialValue is always up-to-date in case the observable changes.
-   */
-  useEffect(() => {
-    initialValueRef.current = getValue(initialValue) as ObservedValueOf<ObservableType>
-  }, [initialValue])
+    // Eagerly subscribe to sync set `entry.currentValue` to what the observable returns, and keep the observable alive until the component unmounts.
+    const subscription = entry.observable.subscribe()
+    subscription.unsubscribe()
+
+    cache.set(observable, entry as CacheRecord<ObservedValueOf<ObservableType>>)
+  }
 
   const store = useMemo(() => {
-    if (!cache.has(observable)) {
-      const entry: Partial<CacheRecord<ObservedValueOf<ObservableType>>> = {
-        snapshot: initialValueRef.current,
-      }
-      entry.observable = observable.pipe(
-        map((value) => ({snapshot: value, error: undefined})),
-        catchError((error) => of({snapshot: undefined, error})),
-        tap(({snapshot, error}) => {
-          entry.snapshot = snapshot
-          entry.error = error
-        }),
-        // Note: any value or error emitted by the provided observable will be mapped to the cache entry's mutable state
-        // and the observable is thereafter only used as a notifier to call `onStoreChange`, hence the `void` return type.
-        map((value) => void value),
-        // Ensure that the cache entry is deleted when the observable completes or errors.
-        finalize(() => cache.delete(observable)),
-        share({resetOnRefCountZero: () => timer(0, asapScheduler)}),
-      )
-
-      // Eagerly subscribe to sync set `entry.currentValue` to what the observable returns, and keep the observable alive until the component unmounts.
-      const subscription = entry.observable.subscribe()
-      subscription.unsubscribe()
-
-      cache.set(observable, entry as CacheRecord<ObservedValueOf<ObservableType>>)
-    }
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const instance = cache.get(observable)!
-
     return {
       subscribe: (onStoreChange: () => void) => {
         const subscription = instance.observable.subscribe(onStoreChange)
@@ -103,6 +90,8 @@ export function useObservable<ObservableType extends Observable<any>, InitialVal
   return useSyncExternalStore<ObservedValueOf<ObservableType>>(
     store.subscribe,
     store.getSnapshot,
-    typeof initialValueRef.current === 'undefined' ? undefined : () => initialValueRef.current,
+    typeof initialValue === 'undefined'
+      ? undefined
+      : () => getValue(initialValue) as ObservedValueOf<ObservableType>,
   )
 }
