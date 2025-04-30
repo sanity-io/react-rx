5018b87d4ee285e1aaee0aba5ec1ea881619388d
Cody Olsen <stipsan@gmail.com>
2025-04-30 12:37:15 +0200

    chore(deps): upgrade TypeScript to version 5.8.3

--- src/useObservable.ts
+++ src/useObservable.ts
@@ -12,7 +12,9 @@ import {
 import {map, tap} from 'rxjs/operators'
 
 function getValue<T>(value: T): T extends () => infer U ? U : T {
-  return typeof value === 'function' ? value() : value
+  return (typeof value === 'function' ? (value as () => any)() : value) as T extends () => infer U
+    ? U
+    : T
 }
 
 interface ObservableState<T> {
