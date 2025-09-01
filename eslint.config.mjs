52826002639be04f0ec2402947eec6fd1d796196
Cody Olsen <stipsan@gmail.com>
2025-09-01 19:33:42 +0200

    chore: update linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import {defineConfig, globalIgnores} from 'eslint/config'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
 import reactPlugin from 'eslint-plugin-react'
 import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import reactHooksPluginWithUseEffectEvent from 'eslint-plugin-react-hooks-with-use-effect-event'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import tseslint from 'typescript-eslint'
 
@@ -11,18 +12,19 @@ export default defineConfig([
   js.configs.recommended,
   tseslint.configs.recommended,
   eslintConfigPrettier,
+  ...reactHooksPlugin.configs['flat/recommended'],
   {
     plugins: {
       'simple-import-sort': simpleImportSort,
       'react': reactPlugin,
-      'react-hooks': reactHooksPlugin,
+      'react-hooks-with-use-effect-event': reactHooksPluginWithUseEffectEvent,
     },
     rules: {
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'error',
-      'react-hooks/react-compiler': 'error',
+      // This rule understands useEffectEvent, unlike the original react-hooks plugin
+      'react-hooks-with-use-effect-event/exhaustive-deps': 'error',
+      'react-hooks/exhaustive-deps': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
     },
   },
