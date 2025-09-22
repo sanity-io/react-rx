89efb0e75951a1e56978eb7c5b646e3bda93b5ef
Cody Olsen <stipsan@gmail.com>
2025-09-22 16:04:47 +0200

    chore: fix type test

--- src/__tests__/useObservable.test-d.ts
+++ src/__tests__/useObservable.test-d.ts
@@ -15,13 +15,13 @@ test('useObservable with no initial value can be undefined', () => {
 test('return type of useObservable with initial value is not undefined', () => {
   const observable = of('foo')
   //@ts-expect-error - because initial value is given, the return type can never be undefined
-  expectTypeOf(useObservable(observable, {initialValue: 'bar'})).toEqualTypeOf<string | undefined>()
+  expectTypeOf(useObservable(observable, 'bar')).toEqualTypeOf<string | undefined>()
 })
 
 test('useObservable with initial value if a different type returns a union of the observed type and the initial value type', () => {
   const observable = of('foo')
 
-  expectTypeOf(useObservable(observable, {initialValue: 1})).toEqualTypeOf<string | number>()
-  expectTypeOf(useObservable(observable, {initialValue: () => 1})).toEqualTypeOf<string | number>()
-  expectTypeOf(useObservable(observable, {initialValue: 'foo'})).toEqualTypeOf<string>()
+  expectTypeOf(useObservable(observable, 1)).toEqualTypeOf<string | number>()
+  expectTypeOf(useObservable(observable, () => 1)).toEqualTypeOf<string | number>()
+  expectTypeOf(useObservable(observable, 'foo')).toEqualTypeOf<string>()
 })
