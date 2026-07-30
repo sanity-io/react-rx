8665f365c3887bd1381a59731459e340632ddde0
Cody Olsen <81981+stipsan@users.noreply.github.com>
2026-07-30 09:01:46 +0200

    Update ExampleSandpack.tsx
    Signed-off-by: Cody Olsen <81981+stipsan@users.noreply.github.com>

--- website/src/components/ExampleSandpack.tsx
+++ website/src/components/ExampleSandpack.tsx
@@ -4,10 +4,6 @@ import Sandpack from '@/components/Sandpack'
 import {readReactRxDist} from '@/utils/readExample'
 
 
-/**
- * Server wrapper that loads the local react-rx build for Sandpack in development.
- * Example source files should be read with `readExample` and passed as `files`.
-*/
 export default function ExampleSandpack(
   props: Omit<ComponentProps<typeof Sandpack>, 'reactRxSource'>,
 ) {
